fa7ff24c08e34dfbdc131aa95471004ff109a85c
Sergio <38168163+FundacionBlockchainChile@users.noreply.github.com>
2021-10-21 00:23:08 -0300

    routes edited to convention

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
-  get 'apartment/index'
-  get 'apartment/show'
-  get 'apartment/new'
-  post 'apartment/create'
-  get 'apartment/edit'
-  get 'apartment/delete'
-  get 'building/index'
-  get 'building/show'
-  get 'building/new'
-  post 'building/create'
-  get 'building/edit'
-  get 'building/delete'
+  # apartments
+  get 'apartments/index' => "apartment#index"
+  get 'apartments/new' => "apartment#new"
+  post 'apartments/create' => "apartment#create"
+  get 'apartments/show' => "apartment#show"
+  get 'apartments/edit' => "apartment#edit"
+  patch 'apartments/:id' => "apartment#update"
+  delete 'apartments/:id' => "apartment#delete"
+  # buildings
+  get 'buildings/index' => "building#index"
+  get 'buildings/show' => "building#new"
+  get 'buildings/new' => "building#create"
+  post 'buildings/create' => "building#show"
+  patch 'buildings/edit' => "building#update"
+  delete 'buildings/delete' => "building#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
